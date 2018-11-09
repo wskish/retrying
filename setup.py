c67afd2dd14964f327043635aa0ce2ebc1ea89ac
Bill Kish <bill@cogniac.co>
2018-11-09 11:37:49 -0800

    bump version for internal use

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ with open('requirements.txt') as file_requirements:
 
 settings.update(
     name='retrying',
-    version='1.3.4-dev',
+    version='2.0.0',
     description='Retrying',
     long_description=readme + '\n\n' + history,
     author='Ray Holder',
